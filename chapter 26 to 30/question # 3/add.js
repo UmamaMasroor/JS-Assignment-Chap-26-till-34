a93db4ac471153e879044fe3c9fce52b1762d775
Umama Masroor <umamafatima319@gmail.com>
2023-06-20 05:28:31 -0700

    question 3

---
+++ chapter 26 to 30/question # 3/add.js
@@ -0,0 +1,8 @@
+// question 3
+// Write a program that displays the absolute value of a
+// number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+let number = prompt("Enter a number");
+num = Math.abs(number);
+document.write("The absolute value of " + number + " is " + num);
